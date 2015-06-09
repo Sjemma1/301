8b6e5f18de987522917620d3e45365de7220d2c0
Lukas Feitsma <lukasfeitsma@145.191.028.145.hva.nl>
2015-06-09 13:37:46 +0200

    some small changes

--- packages/telescope-theme-301/client/js/post_profile_weld.js
+++ packages/telescope-theme-301/client/js/post_profile_weld.js
@@ -1,15 +1,3 @@
 Template[getTemplate('postProfileWeld')].helpers({
-  // needsDisplayName: function () {
-  //   // If no Display Name OR if it matches username
-  //   if(!this.profile.name || this.username === this.profile.name)
-  //     return true
-  // },
-  // authorName: function(){
-  //   return getAuthorName(this);
-  // },
-  // profileUrl: function(){
-  //   var user = Meteor.users.findOne(this.userId);
-  //   if(user)
-  //     return getProfileUrl(user);
-  // }
+
 });
