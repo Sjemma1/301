256ba1b23b22d568246f50ca77e9fe96470b1a5c
Lukas Feitsma <lukasfeitsma@MacBook-Pro-van-Lukas.local>
2015-06-18 10:28:04 +0200

    add mouse wheel

--- packages/telescope-theme-301/client/js/post_page_weld.js
+++ packages/telescope-theme-301/client/js/post_page_weld.js
@@ -52,3 +52,85 @@ return getTemplate('postProfileWeld');
       return getDribbble(user);
   }
 });
+
+
+// Router.map(function() {
+//   this.route('public', {
+//     path: '/',
+//     template: 'public'
+//   }),
+//   this.route('admin', {
+//     path: '/admin',
+//     template: 'admin',
+//     action: function() {
+//       var self = this;
+//       $.getScript('/js/moment.min.js', function(data, textStatus, jqxhr) {
+//         if(jqxhr.status === 200) {
+//           self.render();
+//         }
+//      });
+//     }
+//   })
+// });
+
+//
+// Router.map( function () {
+//   this.route('posts_list_weld',{
+//     waitOn: IRLibLoader.load('https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.12/jquery.mousewheel.js')
+//   });
+// });
+
+// Meteor.Router.filters({
+//   'isCustomScriptLoaded': function(page) {
+//     if(!Session.equals('isCustomScriptLoaded', true)){
+//         Session.set('isCustomScriptLoaded', true);
+//         var script = document.createElement('script');
+//         script.type = 'text/javascript';
+//         script.async = true;
+//         script.src = "<EXTERNAL SCRIPT URL>";
+//
+//         var firstScript = document.getElementsByTagName('script')[0];
+//         firstScript.parentNode.insertBefore(script, firstScript);
+//     }
+//   }
+// });
+
+// Meteor.Router.filter('isCustomScriptLoaded',{only: ['posts_list_weld'] });
+//
+// Template.posts_list_weld.created = function() {
+//   scriptInsert = function () {
+//     // $('head').append('<script type="text/javascript" src="/mousewheel.js">');
+//     $(document).ready(function() {
+//
+//       $('html, body, *').mousewheel(function(e, delta) {
+//         this.scrollLeft -= (delta * 40);
+//         e.preventDefault();
+//       });
+//
+//     });
+//   }
+// }
+
+// Template.posts_list_weld.helpers({
+//   posts: function () {
+//     $('html, body, *').mousewheel(function(e, delta) {
+//                      this.scrollLeft -= (delta * 40);
+//                      e.preventDefault();
+//                    });
+//                  });
+//     return Posts;
+//   }
+// });
+
+
+// Template.posts_list_weld.rendered=function(){
+//     var initializeOnlyOnce=_.once(function(){
+//         $(document).ready(function() {
+//               $('html, body, *').mousewheel(function(e, delta) {
+//                 this.scrollLeft -= (delta * 40);
+//                 e.preventDefault();
+//               });
+//             });
+//     });
+//     initializeOnlyOnce();
+// };
